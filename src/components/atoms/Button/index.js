62be99cde67d7301a1ce5f8dc4840714469bcd9f
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-12-19 20:32:23 +0600

    updated readme

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,14 +1,18 @@
 import styled from '@emotion/styled';
 
+const selectColor = props => props.color
+  ? props.theme.colors[props.color]
+  : props.theme.colors.primary;
+
 /** @component */
 export default styled.button`
   margin: 0;
   padding: 0 20px;
   border-radius: 3px;
-  border: 1px solid ${props => props.color ? props.theme.colors[props.color] : props.theme.colors.primary};
+  border: 1px solid ${selectColor};
   cursor: pointer;
   color: white;
-  background: ${props => props.color ? props.theme.colors[props.color] : props.theme.colors.primary};
+  background: ${selectColor};
   height: 38px;
   font-size: 0.9rem;
   outline: none;
